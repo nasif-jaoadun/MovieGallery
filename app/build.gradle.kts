3afdc689e4d50f2af33335f42d2ed9e300812613
nasif-jaoadun <nasif_online@yahoo.com>
2025-02-25 23:51:30 +0600

    Parse Json Strings with Moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.8.0")
 }
--- app/src/main/java/com/jnasif/moviegallery/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jnasif/moviegallery/ui/main/MainViewModel.kt
@@ -4,13 +4,26 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.jnasif.moviegallery.LOG_TAG
-import com.jnasif.moviegallery.R
+import com.jnasif.moviegallery.data.MovieDetails
+import com.jnasif.moviegallery.data.Movies
 import com.jnasif.moviegallery.utilities.FileHelper
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 
 class MainViewModel(app : Application) : AndroidViewModel(app) {
     init {
 //        val text = FileHelper.getTextFromResources(app,R.raw.tmdb_movie_discovery)
         val text = FileHelper.getTextFromAssets(app,"tmdb_movie_discovery.json")
-        Log.i(LOG_TAG, text)
+        parseText(text)
+    }
+
+    fun parseText(text : String){
+        val moshi = Moshi.Builder().build()
+        val jsonAdapter = moshi.adapter(Movies::class.java)
+        val movieJson = jsonAdapter.fromJson(text)
+        val movieData = movieJson?.results
+        for (movie in movieData ?: emptyList()){
+            Log.i(LOG_TAG, "${movie.title} (\$${movie.vote_count})")
+        }
     }
 }
