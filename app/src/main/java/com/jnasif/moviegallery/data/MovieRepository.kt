6f3606f42e27ebebde499edf035c7f550ebd36aa
nasif-jaoadun <nasif_online@yahoo.com>
2025-03-03 02:05:28 +0600

    Acquire data in a repository class

---
+++ app/src/main/java/com/jnasif/moviegallery/data/MovieRepository.kt
@@ -0,0 +1,20 @@
+package com.jnasif.moviegallery.data
+
+import android.content.Context
+import android.util.Log
+import com.jnasif.moviegallery.LOG_TAG
+import com.jnasif.moviegallery.utilities.FileHelper
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+class MovieRepository {
+
+    fun getMovieData(context : Context) : List<MovieDetails>{
+//        val text = FileHelper.getTextFromResources(app,R.raw.tmdb_movie_discovery)
+        val text = FileHelper.getTextFromAssets(context,"tmdb_movie_discovery.json")
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val jsonAdapter = moshi.adapter(Movies::class.java)
+        val movieJson = jsonAdapter.fromJson(text)
+        return movieJson?.listOfMoviesWithDetails ?: emptyList()
+    }
+}
--- app/src/main/java/com/jnasif/moviegallery/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jnasif/moviegallery/ui/main/MainViewModel.kt
@@ -4,25 +4,20 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.jnasif.moviegallery.LOG_TAG
+import com.jnasif.moviegallery.data.MovieRepository
 import com.jnasif.moviegallery.data.Movies
 import com.jnasif.moviegallery.utilities.FileHelper
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainViewModel(app : Application) : AndroidViewModel(app) {
+    private val dataRepo = MovieRepository()
     init {
-//        val text = FileHelper.getTextFromResources(app,R.raw.tmdb_movie_discovery)
-        val text = FileHelper.getTextFromAssets(app,"tmdb_movie_discovery.json")
-        parseText(text)
-    }
-
-    fun parseText(text : String){
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val jsonAdapter = moshi.adapter(Movies::class.java)
-        val movieJson = jsonAdapter.fromJson(text)
-        val movieData = movieJson?.listOfMoviesWithDetails
-        for (movie in movieData ?: emptyList()){
+        val movieData = dataRepo.getMovieData(app)
+        for (movie in movieData){
             Log.i(LOG_TAG, "${movie.title} (\$${movie.vote_count})")
         }
     }
+
+
 }
