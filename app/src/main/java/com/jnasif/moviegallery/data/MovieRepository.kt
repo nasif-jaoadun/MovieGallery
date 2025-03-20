82c7e30b0385799e1f7f98e02449431f7e4b3c22
nasif-jaoadun <nasif_online@yahoo.com>
2025-03-21 02:25:26 +0600

    2.3 Network Permission and Status

--- app/src/main/java/com/jnasif/moviegallery/data/MovieRepository.kt
+++ app/src/main/java/com/jnasif/moviegallery/data/MovieRepository.kt
@@ -2,7 +2,10 @@ package com.jnasif.moviegallery.data
 
 import android.app.Application
 import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.jnasif.moviegallery.LOG_TAG
 import com.jnasif.moviegallery.utilities.FileHelper
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -11,6 +14,7 @@ class MovieRepository(val app : Application) {
     val movieDetailsData = MutableLiveData<List<MovieDetails>>()
     init {
         getMovieData()
+        Log.i(LOG_TAG, "Network Available: ${networkAvailable()}")
     }
     fun getMovieData(){
 //        val text = FileHelper.getTextFromResources(app,R.raw.tmdb_movie_discovery)
@@ -19,5 +23,12 @@ class MovieRepository(val app : Application) {
         val jsonAdapter = moshi.adapter(Movies::class.java)
         val movieJson = jsonAdapter.fromJson(text)
         movieDetailsData.value = movieJson?.listOfMoviesWithDetails ?: emptyList()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun networkAvailable(): Boolean {
+        val connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnectedOrConnecting ?: false
     }
 }
